1df3d43d1fb9c828f744304513ef4d3d494e4404
Dmitriy <dimusy@mail.ru>
2020-07-22 14:46:15 +0300

    first commit

---
+++ client/src/main/java/Main.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(event -> {
+            try {
+                Controller.socket.getOutputStream().write("close".getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Platform.exit();
+        });
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
